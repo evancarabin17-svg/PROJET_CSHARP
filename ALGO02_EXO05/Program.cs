c28a1d956830e881d57d898832b13259f5707627
CARABIN EVAN <evan.carabin17@gmail.com>
2026-03-20 21:30:57 +0100

    ALGO02_EXO05

---
+++ ALGO02_EXO05/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ALGO02_EXO05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tmp = 0;
+
+            Console.WriteLine("Saisir x : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Saisir y : ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            tmp = x;
+            x = y;
+            y = tmp;
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+        }
+    }
+}
--- ALGO02_EXO05/obj/Debug/net10.0/ALGO02_EXO05.AssemblyInfo.cs
+++ ALGO02_EXO05/obj/Debug/net10.0/ALGO02_EXO05.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ALGO02_EXO02")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ALGO02_EXO05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41c95f5847561cf83c889f38d275d3ac8be816ea")]
-[assembly: System.Reflection.AssemblyProductAttribute("ALGO02_EXO02")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ALGO02_EXO02")]
+[assembly: System.Reflection.AssemblyProductAttribute("ALGO02_EXO05")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ALGO02_EXO05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
