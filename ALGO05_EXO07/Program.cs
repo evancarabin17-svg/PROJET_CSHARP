0d507f66ee32aeee850c34fa3c4fcb710fd73f25
CARABIN EVAN <evan.carabin17@gmail.com>
2026-03-28 07:42:19 +0100

    ALGO05_EXO07

---
+++ ALGO05_EXO07/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ALGO05_EXO07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int somme = 0;
+            int nbrEleves = 0;
+            bool continuer = true;
+
+            while (continuer == true)
+            {
+                Console.WriteLine("Note ? :");
+                int note = Convert.ToInt32(Console.ReadLine());
+                somme = somme + note;
+                nbrEleves = nbrEleves + 1;
+                Console.WriteLine("saisir true pour continuer ou flase pour arrêter");
+                continuer = bool.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("La moyenne de la classe est de : " + somme / nbrEleves);
+        }
+    }
+}
