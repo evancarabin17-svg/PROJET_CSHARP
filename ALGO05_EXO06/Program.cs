959fc94f8afb5f6f8c3a13bedba891b6e3957461
CARABIN EVAN <evan.carabin17@gmail.com>
2026-03-28 07:26:27 +0100

    ALGO05_EXO06

---
+++ ALGO05_EXO06/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ALGO05_EXO06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int somme = 0;
+            int nbrNotes = 0;
+
+            Console.WriteLine("Combien d'élèves composent la classe ?");
+            int nbrEleves = Convert.ToInt32(Console.ReadLine());
+            while (nbrNotes < nbrEleves)
+            {
+                Console.WriteLine("Note ? :");
+                int note = Convert.ToInt32(Console.ReadLine());
+                somme = somme + note;
+                nbrNotes = nbrNotes + 1;
+            }
+            Console.WriteLine("La moyenne de la classe est de : " + somme / nbrEleves);
+        }
+    }
+}
