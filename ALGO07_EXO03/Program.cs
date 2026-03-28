048ad4e5a6accb43df24d78d28b6ecdfa7c29f5f
CARABIN EVAN <evan.carabin17@gmail.com>
2026-03-28 13:15:27 +0100

    ALGO07_EXO03

---
+++ ALGO07_EXO03/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ALGO07_EXO03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int decremente (int nb)
+            {
+                return nb - 1;
+            }
+        }
+    }
+}
